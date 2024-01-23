b53ef8260512723ffcae24f90adc7fdc40d36651
Romy0110 <romydev01@gmail.com>
2024-01-23 12:59:30 +0100

    Update ProvideRoutes.jsx

--- hrnet/src/routes/ProvideRoutes.jsx
+++ hrnet/src/routes/ProvideRoutes.jsx
@@ -1,6 +1,7 @@
 import {Routes, Route} from "react-router";
 import Home from "../ui/Home";
 import List from "../ui/List";
+import Error from "../components/Error";
 const ProvideRoutes = () => {
 
 
@@ -8,6 +9,7 @@ const ProvideRoutes = () => {
     <Routes>
     <Route path = "/" element = {<Home/>}/>
     <Route path = "/list" element = {<List/>}/>
+    <Route path = "/*" element = {<Error/>}/>
     
     </Routes>
 
